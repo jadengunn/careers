53cfe0549c87ceab11fb10fbd028b379539fa7b0
jadengunn <jtgunn@vt.edu>
2023-04-11 17:47:51 -0400

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+var deloitte = document.querySelector('img[alt="Deloitte"]');
+var ey = document.querySelector('img[alt="EY"]');
+var kpmg = document.querySelector('img[alt="KPMG"]');
+var kearney = document.querySelector('img[alt="Kearney and Company"]');
+
+deloitte.addEventListener('click', function() {
+	window.location.href = 'deloitte.html';
+});
+
+ey.addEventListener('click', function() {
+	window.location.href = 'ey.html';
+});
+
+kpmg.addEventListener('click', function() {
+	window.location.href = 'kpmg.html';
+});
+
+kearney.addEventListener('click', function() {
+	window.location.href = 'kearney.html';
+});
